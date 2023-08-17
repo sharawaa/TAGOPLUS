38975c490d113ae9f172d8f798c94413559f8cec
Sharawmnyambuu <sharawnyambuuurtnasan@gmail.com>
2023-08-17 09:09:38 +0800

    push

---
+++ assets/js/chart.js
@@ -0,0 +1,49 @@
+const dataset = [75];
+const colors = ["#3498db", "#e74c3c"];
+const labels = ["Label 1"];
+
+const width = 200;
+const height = 200;
+
+const svg = d3
+  .select("#doughnutChart")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height);
+
+const radius = Math.min(width, height) / 2;
+
+const pie = d3.pie().value((d) => d);
+
+const arc = d3
+  .arc()
+  .innerRadius(radius * 0.7)
+  .outerRadius(radius);
+
+const arcs = pie(dataset);
+
+const g = svg
+  .append("g")
+  .attr("transform", `translate(${width / 2},${height / 2})`);
+
+g.selectAll("path")
+  .data(arcs)
+  .enter()
+  .append("path")
+  .attr("d", arc)
+  .attr("fill", (d, i) => colors[i]);
+
+// Adding labels
+const labelGroup = g
+  .selectAll(".label")
+  .data(arcs)
+  .enter()
+  .append("g")
+  .attr("class", "label");
+
+labelGroup
+  .append("text")
+  .attr("transform", (d) => `translate(${arc.centroid(d)})`)
+  .attr("dy", ".35em")
+  .style("text-anchor", "middle")
+  .text((d, i) => labels[i]);
